ac3c7a0c5c66604c9f9016810abea10c66937729
AdamYuan <y13916619121@126.com>
2025-02-10 20:48:44 +0800

    Move indLight splat algorithm calls to GS3DIndLight.cpp

--- Renderer/IndLight/3DGS/GS3DIndLight.cpp
+++ Renderer/IndLight/3DGS/GS3DIndLight.cpp
@@ -5,6 +5,8 @@
 #include "GS3DIndLight.hpp"
 
 #include "GS3DIndLightSplat.hpp"
+#include "GS3DIndLightAlgo.hpp"
+#include "../../../Algorithm/MeshVHBVH.hpp"
 
 namespace GSGI
 {
@@ -22,8 +24,37 @@ void GS3DIndLight::update(RenderContext* pRenderContext, bool isActive, bool isS
         std::vector<GS3DIndLightSplat> splats;
         for (const auto& pMesh : pDefaultStaticScene->getMeshes())
         {
-            auto meshSplats = GS3DIndLightSplat::fromMesh(pMesh, mConfig.splatsPerMesh);
+            auto meshView = GMeshView{pMesh};
+            MeshBVH<AABB> meshBVH;
+            auto meshSplats = GS3DIndLightSplat::loadMesh(pMesh, mConfig.splatsPerMesh);
+            if (meshSplats.empty())
+            {
+                if (meshBVH.isEmpty())
+                    meshBVH = MeshBVH<AABB>::build<MeshVHBVHBuilder>(meshView);
+                meshSplats = GS3DIndLightAlgo::getSplatsFromMeshFallback(meshView, meshBVH, mConfig.splatsPerMesh);
+                GS3DIndLightSplat::persistMesh(pMesh, meshSplats);
+            }
             splats.insert(splats.end(), meshSplats.begin(), meshSplats.end());
+
+            /* {
+                if (meshBVH.isEmpty())
+                    meshBVH = MeshBVH<AABB>::build<MeshVHBVHBuilder>(meshView);
+
+                std::vector<std::vector<uint32_t>> primSplatIDs =
+                    GS3DIndLightAlgo::getPrimitiveIntersectedSplatIDs(meshView, meshBVH, meshSplats);
+
+                uint32_t greater16Cnt = 0, greater8Cnt = 0, equal0Cnt = 0;
+                for (uint i = 0; i < pMesh->getPrimitiveCount(); ++i)
+                {
+                    if (primSplatIDs[i].size() > 16)
+                        ++greater16Cnt;
+                    if (primSplatIDs[i].size() > 8)
+                        ++greater8Cnt;
+                    if (primSplatIDs[i].empty())
+                        ++equal0Cnt;
+                }
+                fmt::println(">16: {}, >8: {}, =0: {}", greater16Cnt, greater8Cnt, equal0Cnt);
+            } */
         }
 
         mpSplatBuffer = getDevice()->createStructuredBuffer(
